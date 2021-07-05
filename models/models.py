8861ee82a19326d24a773804a0a93445b35e5916
mogwebz <erlang.ug@gmail.com>
2021-07-05 05:02:49 +0300

    Test Module Changes

--- models/models.py
+++ models/models.py
@@ -11,11 +11,12 @@ class test_module(models.Model):
     # value = fields.Integer()
     # value2 = fields.Float(compute="_value_pc", store=True)
     # description = fields.Text()
+    @api.depends('field_one', 'field_two')
+    def _value_pc(self):
+        for rec in self:
+            rec.field_3 = rec.field_one / rec.field_two if (rec.field_one > 0 or rec.field_one < 0) and (rec.field_two  > 0 or rec.field_two  < 0) else 0
     field_one = fields.Integer('Field One', default=0.25)
     field_two = fields.Integer('Field Two', default=0.25)
-    field_3 = fields.Float('Field Three')
+    field_3 = fields.Float('Field Three', compute=_value_pc)
 
-    @api.depends('value')
-    def _value_pc(self):
-        for record in self:
-            record.value2 = float(record.value) / 100
+    
